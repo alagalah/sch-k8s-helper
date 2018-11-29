0b78d16a39674ed199d10bc9ad19ec7c23a5e41b
Keith Burns <keith@streamsets.com>
2018-11-29 09:19:25 -0800

    Testing for curl and DIR stuff
    Signed-off-by: Keith Burns <keith@streamsets.com>

---
+++ gcp/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+#  Copyright 2018 StreamSets Inc.
+#
+
+# Get the directory the script is from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo ${SCRIPT_DIR}
